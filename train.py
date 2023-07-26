ca463e49b56afe2ff5dbdba885acfa75be2e5d12
VuKieuAnh <vukieuanh.hnue@gmail.com>
2023-07-26 22:57:04 +0700

    init

---
+++ train.py
@@ -0,0 +1,25 @@
+from random import random
+# from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+from RandomForest import RandomForest
+
+data = df = pd.read_csv("data/winequality-red.csv")
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=1234
+)
+
+def accuracy(y_true, y_pred):
+    accuracy = np.sum(y_true == y_pred) / len(y_true)
+    return accuracy
+
+clf = RandomForest(n_trees=20)
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_test)
+
+acc =  accuracy(y_test, predictions)
+print(acc)
